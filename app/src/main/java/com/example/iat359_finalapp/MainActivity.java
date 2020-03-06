93ff68fa17a8083656bea43cbe0124bf137d500f
Matilda Chiang <chiang.matilda@gmail.com>
2020-03-06 13:53:37 -0800

    March 6, 2020 Add activities
    Need to work on maps

--- app/src/main/java/com/example/iat359_finalapp/MainActivity.java
+++ app/src/main/java/com/example/iat359_finalapp/MainActivity.java
@@ -2,16 +2,49 @@ package com.example.iat359_finalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button startButton, historyButton, settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("HI");
-        System.out.println("BYE");
+        //start buton initalize
+        startButton = (Button)findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+
+        //history button initialize
+        historyButton = (Button)findViewById(R.id.history_button);
+        historyButton.setOnClickListener(this);
+
+        //settings button initialize
+        settingsButton = (Button)findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //if start button is pressed
+        if(v.getId() == R.id.start_button){
+            Intent i = new Intent(MainActivity.this, SetupActivity.class);
+            startActivity(i);
+        }
+        if(v.getId() == R.id.history_button){
+            Intent i = new Intent(MainActivity.this, HistoryActivity.class);
+            startActivity(i);
+        }
+        if(v.getId() == R.id.settings_button){
+            Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(i);
+        }
     }
 }
