a51583139d168f81e3dad1e15d276adaf6ea3825
Karmen <emeraldsuen@gmail.com>
2020-03-27 15:55:12 -0700

    added sharedpref for milestone 2

--- app/src/main/java/com/example/iat359_finalapp/SettingsActivity.java
+++ app/src/main/java/com/example/iat359_finalapp/SettingsActivity.java
@@ -2,13 +2,42 @@ package com.example.iat359_finalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText usernameEditText, passwordEditText;
+    Button submitButton;
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString("username", usernameEditText.getText().toString());
+        editor.putString("password", passwordEditText.getText().toString());
+        Toast.makeText(this, "Username and password saved to Preferences", Toast.LENGTH_LONG).show();
+        editor.commit();
+
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
